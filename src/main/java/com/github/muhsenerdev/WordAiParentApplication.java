33f700e6091fd534f988d40c974c506c77fb87eb
muhsener-be <muhammet.sener@bilgiedu.net>
2025-12-02 18:09:11 +0300

    first commit

---
+++ src/main/java/com/github/muhsenerdev/WordAiParentApplication.java
@@ -0,0 +1,13 @@
+package com.github.muhsenerdev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordAiParentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WordAiParentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/muhsenerdev/WordAiParentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.muhsenerdev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordAiParentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
